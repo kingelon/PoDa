7ca8c0822f43cece73c65d3059352b151d21eb1f
Is Mavip <150976957+kingelon@users.noreply.github.com>
2024-10-07 14:43:39 -0400

    adding sample springboot with main

--- springBoots/basicSpringTest.java
+++ springBoots/basicSpringTest.java
@@ -1,13 +1,25 @@
-package com.example.hbaseapi.controller;
+package com.example.hbaseapi;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class PingController {
+@SpringBootApplication
+public class HBaseApiApplication {
 
-    @GetMapping("/ping")
-    public String ping() {
-        return "Spring Boot application is running!";
+    public static void main(String[] args) {
+        SpringApplication.run(HBaseApiApplication.class, args);
+    }
+
+    @RestController
+    @RequestMapping("/api")
+    public static class SimpleController {
+
+        @GetMapping("/ping")
+        public String ping() {
+            return "Spring Boot application is running!";
+        }
     }
 }
