4aa7ad8f130533d3775d43f20585618191174152
Is Mavip <150976957+kingelon@users.noreply.github.com>
2024-10-07 14:42:19 -0400

    adding sample springboot

---
+++ springBoots/basicSpringTest.java
@@ -0,0 +1,13 @@
+package com.example.hbaseapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "Spring Boot application is running!";
+    }
+}
